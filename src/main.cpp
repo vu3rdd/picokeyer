70dd08161e737e90fda9ccb52ce82d57b9bb3de8
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2022-06-12 17:30:30 +0530

    code compiles

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico_tone.hpp"
 
 enum KSTYPE {IDLE, CHK_DIT, CHK_DAH, KEYED_PREP, KEYED, INTER_ELEMENT };
 
@@ -11,14 +12,6 @@ enum KSTYPE {IDLE, CHK_DIT, CHK_DAH, KEYED_PREP, KEYED, INTER_ELEMENT };
 #define     PDLSWAP    0x08     // 0 for normal, 1 for swap
 #define     IAMBICB    0x10     // 0 for Iambic A, 1 for Iambic B
 #define     IAMBICA    0x00     // 0 for Iambic A.
-
-unsigned char       keyerState;
-unsigned char       iambicMode = IAMBICA;       // or 0 for Iambic A
-unsigned char       keyerControl;
-unsigned int        wpm = 25;
-unsigned long       ditTime;                    // No. milliseconds per dit
-unsigned int        paddleReverse = 1;
-unsigned int        sidetone_freq = 800;        // 800 Hz
 
 // GPIO pin definitions
 const int   DahPin          = 10;      // Dah paddle input or PTT
@@ -32,99 +25,21 @@ const int   cw_sound_out    = 4;      // square wave out corresponding to the CW
 // global state
 int ptt;                   // the current ptt value
 int key;                   // the current key value
+// tone out pin
+Tone myPlayer(cw_sound_out);
+
+unsigned char       keyerState;
+unsigned char       iambicMode = IAMBICA;       // or 0 for Iambic A
+unsigned char       keyerControl;
+unsigned int        wpm = 25;
+unsigned long       ditTime;                    // No. milliseconds per dit
+unsigned int        paddleReverse = 1;
+unsigned int        sidetone_freq = 800;        // 800 Hz
 
 uint64_t millis() {
     uint64_t us = time_us_64();
 
     return us/1000;
-}
-
-void keyerFunc(void)
-{
-    static unsigned long ktimer;
-
-    // Basic Iambic Keyer
-    // keyerControl contains processing flags and keyer mode bits
-    // Supports Iambic A and B
-    // State machine based, uses calls to millis() for timing.
- 
-    switch (keyerState) {
-    case IDLE:
-        // Wait for direct or latched paddle press
-        if ((gpio_get(DahPin) == 0) ||
-	    (gpio_get(DitPin) == 0) ||
-	    (keyerControl & 0x03)) {
-            update_PaddleLatch();
-            keyerState = CHK_DIT;
-        }
-        break;
- 
-    case CHK_DIT:
-        // See if the dit paddle was pressed
-        if (keyerControl & DIT_L) {
-            keyerControl |= DIT_PROC;
-            ktimer = ditTime;
-            keyerState = KEYED_PREP;
-        }
-        else {
-            keyerState = CHK_DAH;
-        }
-        break;
- 
-    case CHK_DAH:
-        // See if dah paddle was pressed
-        if (keyerControl & DAH_L) {
-            ktimer = ditTime*3;
-            keyerState = KEYED_PREP;
-        }
-        else {
-            keyerState = IDLE;
-        }
-        break;
- 
-    case KEYED_PREP:
-        // Assert key down, start timing, state shared for dit or dah
-        gpio_put(ledPin, true);         // turn the LED on
-
-        // turn on the sound on the sidetone pin
-        tone(cw_sound_out, sidetone_freq);
-
-	ktimer += millis();                 // set ktimer to interval end time
-        keyerControl &= ~(DIT_L + DAH_L);   // clear both paddle latch bits
-        keyerState = KEYED;                 // next state
-        break;
- 
-    case KEYED:
-        // Wait for timer to expire
-        if (millis() > ktimer) {            // are we at end of key down ?
-            gpio_put(ledPin, false);      // turn the LED off
-
-	    // turn off the sound
-	    noTone(cw_sound_out);
-
-	    ktimer = millis() + ditTime;    // inter-element time
-            keyerState = INTER_ELEMENT;     // next state
-        }
-        else if (keyerControl & IAMBICB) {
-            update_PaddleLatch();           // early paddle latch in Iambic B mode
-        }
-        break;
- 
-    case INTER_ELEMENT:
-        // Insert time between dits/dahs
-        update_PaddleLatch();               // latch paddle state
-        if (millis() > ktimer) {            // are we at end of inter-space ?
-            if (keyerControl & DIT_PROC) {             // was it a dit or dah ?
-                keyerControl &= ~(DIT_L + DIT_PROC);   // clear two bits
-                keyerState = CHK_DAH;                  // dit done, check for dah
-            }
-            else {
-                keyerControl &= ~(DAH_L);              // clear dah latch
-                keyerState = IDLE;                     // go idle
-            }
-        }
-        break;
-    }
 }
 
 void update_PaddleLatch()
@@ -177,6 +92,94 @@ void setupIambicMode(int modeB) {
   setupIambic(1);
 }
 
+void keyerFunc(void)
+{
+    static unsigned long ktimer;
+
+    // Basic Iambic Keyer
+    // keyerControl contains processing flags and keyer mode bits
+    // Supports Iambic A and B
+    // State machine based, uses calls to millis() for timing.
+ 
+    switch (keyerState) {
+    case IDLE:
+        // Wait for direct or latched paddle press
+        if ((gpio_get(DahPin) == 0) ||
+	    (gpio_get(DitPin) == 0) ||
+	    (keyerControl & 0x03)) {
+            update_PaddleLatch();
+            keyerState = CHK_DIT;
+        }
+        break;
+ 
+    case CHK_DIT:
+        // See if the dit paddle was pressed
+        if (keyerControl & DIT_L) {
+            keyerControl |= DIT_PROC;
+            ktimer = ditTime;
+            keyerState = KEYED_PREP;
+        }
+        else {
+            keyerState = CHK_DAH;
+        }
+        break;
+ 
+    case CHK_DAH:
+        // See if dah paddle was pressed
+        if (keyerControl & DAH_L) {
+            ktimer = ditTime*3;
+            keyerState = KEYED_PREP;
+        }
+        else {
+            keyerState = IDLE;
+        }
+        break;
+ 
+    case KEYED_PREP:
+        // Assert key down, start timing, state shared for dit or dah
+        gpio_put(ledPin, true);         // turn the LED on
+
+        // turn on the sound on the sidetone pin
+        myPlayer.tone(sidetone_freq);
+
+	ktimer += millis();                 // set ktimer to interval end time
+        keyerControl &= ~(DIT_L + DAH_L);   // clear both paddle latch bits
+        keyerState = KEYED;                 // next state
+        break;
+ 
+    case KEYED:
+        // Wait for timer to expire
+        if (millis() > ktimer) {            // are we at end of key down ?
+            gpio_put(ledPin, false);      // turn the LED off
+
+	    // turn off the sound
+	    myPlayer.stop();
+
+	    ktimer = millis() + ditTime;    // inter-element time
+            keyerState = INTER_ELEMENT;     // next state
+        }
+        else if (keyerControl & IAMBICB) {
+            update_PaddleLatch();           // early paddle latch in Iambic B mode
+        }
+        break;
+ 
+    case INTER_ELEMENT:
+        // Insert time between dits/dahs
+        update_PaddleLatch();               // latch paddle state
+        if (millis() > ktimer) {            // are we at end of inter-space ?
+            if (keyerControl & DIT_PROC) {             // was it a dit or dah ?
+                keyerControl &= ~(DIT_L + DIT_PROC);   // clear two bits
+                keyerState = CHK_DAH;                  // dit done, check for dah
+            }
+            else {
+                keyerControl &= ~(DAH_L);              // clear dah latch
+                keyerState = IDLE;                     // go idle
+            }
+        }
+        break;
+    }
+}
+
 int main() 
 {
     stdio_init_all();
@@ -198,8 +201,9 @@ int main()
 
     gpio_put(LED_PIN, 0);
 
-    while (1) 
-    {
+    myPlayer.init(TONE_NON_BLOCKING);
+
+    while (1) {
 	keyerFunc();
     }
 }
